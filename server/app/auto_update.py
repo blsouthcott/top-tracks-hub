dfce052afc18ddca588addaf2e53a2a929cb78d1
Ben Southcott <benjamin.southcott@gmail.com>
2023-07-18 22:54:31 -0600

    fix auto-updating functionality (not in use)

--- server/app/auto_update.py
+++ server/app/auto_update.py
@@ -3,51 +3,52 @@ import logging
 
 from .models import db, Song, User
 from .scrape_top_tracks import get_pitchfork_top_tracks_html, parse_top_tracks_html
-from .controller import save_new_track_to_db, get_song_by_name_and_artist
-from .spotify import get_spotify_obj, search_spotify_track_id, get_spotify_playlist_id
+from .controller import save_new_track_to_db, add_spotify_track_id_to_song
+from .spotify import get_spotify_obj, get_spotify_playlist_id
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-GENERIC_SPOTIFY_CONFIG_FILE = "tekore.cfg"
-
-
-def check_for_and_add_new_recommended_track():
+def check_for_and_add_new_recommended_track() -> int | None:
+    """
+    checks for the newest recommended track and adds it to the database if it doesn't already exist
+    then searches for the Spotify track ID for that song and adds it if found
+    """
     html = get_pitchfork_top_tracks_html(1)
     new_track = parse_top_tracks_html(html)[0]
-    new_track_id = save_new_track_to_db(new_track, "Pitchfork")
-    if not new_track_id:
+    song_id = save_new_track_to_db(new_track, "Pitchfork")
+    if not song_id:
         return None
 
-    new_song = Song.query.get(new_track_id)
     spotify_obj = get_spotify_obj()
-    spotify_track_id = search_spotify_track_id(spotify_obj, new_song)
-    if not spotify_track_id:
-        # TODO: send an automated email to me telling me I have to manually put in the track ID
-        logging.info(f"Unable to find a Spotify Track ID that matched {new_track}")
-        pass
-    else:
-        new_song.spotify_track_id = spotify_track_id
-        db.session.commit()
-        return spotify_track_id
+    if add_spotify_track_id_to_song(song_id, spotify_obj, db):
+        return song_id
+    
+    logging.info(f"Unable to find a Spotify Track ID that matched {new_track}")
+    return None
+    
 
 
-def update_spotify_playlists(config_dir):
-
-    new_spotify_track_id = check_for_and_add_new_recommended_track()
-    if not new_spotify_track_id:
+def update_spotify_playlists_with_newest_recommended_track(config_dir: str) -> None:
+    """ 
+    checks for the newest recommended track and adds it to every playlist
+    """
+    song_id = check_for_and_add_new_recommended_track()
+    if not song_id:
         logging.info("No new Pitchfork recommended tracks were found.")
         return
+    
+    song = Song.query.get(song_id)
 
-    for file in os.listdir(config_dir):
-        if file != GENERIC_SPOTIFY_CONFIG_FILE:
-            spotify_obj = get_spotify_obj(file)
-            spotify_playlist_id = get_spotify_playlist_id(
-                spotify_obj, User.query.get(file.replace(".cfg", ""))
-            )
-            new_spotify_track_uri = spotify_obj.track(new_spotify_track_id).uri
-            spotify_obj.playlist_add(spotify_playlist_id, [new_spotify_track_uri])
-            logging.info(
-                f"Updated spotify playlist {spotify_playlist_id} with track {new_spotify_track_id}"
-            )
+    config_files = [file for file in os.listdir(config_dir) if "placeholder" not in file]
+    for file in config_files:
+        spotify_obj = get_spotify_obj(file)
+        spotify_playlist_id = get_spotify_playlist_id(
+            spotify_obj, User.query.get(file.replace(".cfg", ""))
+        )
+        new_spotify_track_uri = spotify_obj.track(song.spotify_track_id).uri
+        spotify_obj.playlist_add(spotify_playlist_id, [new_spotify_track_uri])
+        logging.info(
+            f"Updated spotify playlist {spotify_playlist_id} with track {song.spotify_track_id}"
+        )
