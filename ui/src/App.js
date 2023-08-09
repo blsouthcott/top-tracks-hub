11fb91b497b577950fc96ab77f14cf464cb69b3b
Ben Southcott <benjamin.southcott@gmail.com>
2023-08-09 00:05:06 -0600

    remove verify account component

--- ui/src/App.js
+++ ui/src/App.js
@@ -9,9 +9,7 @@ import Contact from './components/contact';
 import ReportAnIssue from './components/reportAnIssue';
 import AddSpotifyTrackId from './components/addSpotifyTrackId';
 import Signup from './components/signUp';
-import VerifyAccount from './components/verifyAccount';
 import UserTopContent from './components/userTopContent';
-import Footer from './components/footer';
 
 
 function App () {
@@ -28,7 +26,6 @@ function App () {
               />
             }/>
             <Route path="/signup" exact element={<Signup />}></Route>
-            <Route path="/verify-account" exact element={<VerifyAccount />}></Route>
             <Route path="/tracks" exact element={<Tracks setIsAuthenticated={setIsAuthenticated} />} />
             <Route path="/add-spotify-track-id/:trackId" exact element={<AddSpotifyTrackId setIsAuthenticated={setIsAuthenticated} />} />
             <Route path="/personalization" exact element={<UserTopContent setIsAuthenticated={setIsAuthenticated} />} />
