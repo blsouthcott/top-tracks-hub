226d7dcaeb190147e94a805ba1a1a4b801f7de96
Ben Southcott <benjamin.southcott@gmail.com>
2023-08-13 16:41:41 -0600

    fix constructing dict from row

--- server/app/api_utils.py
+++ server/app/api_utils.py
@@ -1,11 +1,15 @@
 from datetime import date
 
 def row_to_dict(row):
-    row_dict = row.__dict__
-    row_dict.pop("_sa_instance_state")
-    for key in row_dict:
-        if row_dict[key] and type(row_dict[key]) not in (str, int, date):
-            row_dict[key] = [val.name for val in row_dict[key]]
-        elif row_dict[key] and type(row_dict[key]) is date:
-            row_dict[key] = row_dict[key].strftime("%Y-%m-%d")
+    row_dict = {}
+    for key in row.__dict__:
+        if key == "_sa_instance_state":
+            continue
+        val = row.__dict__[key]
+        if val and type(val) not in (str, int, date):
+            row_dict[key] = [val.name for val in row.__dict__[key]]
+        elif val and type(val) is date:
+            row_dict[key] = val.strftime("%Y-%m-%d")
+        else:
+            row_dict[key] = val
     return row_dict
